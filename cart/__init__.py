eaecf911953ec9bcce5b909f75746453786215c6
SPOORTHI.J <147829893+SPOORTHJ@users.noreply.github.com>
2025-01-25 13:42:20 +0530

    Update __init__.py

--- cart/__init__.py
+++ cart/__init__.py
@@ -1,50 +1,78 @@
 import json
-
-import products
+from typing import List
 from cart import dao
-from products import Product
+from products import Product, get_product
 
 
 class Cart:
-    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
+    def __init__(self, id: int, username: str, contents: List[Product], cost: float):
         self.id = id
         self.username = username
         self.contents = contents
         self.cost = cost
 
-    def load(data):
-        return Cart(data['id'], data['username'], data['contents'], data['cost'])
+    @staticmethod
+    def load(data: dict) -> "Cart":
+        """
+        Loads a Cart object from a dictionary representation.
+        """
+        return Cart(
+            id=data["id"],
+            username=data["username"],
+            contents=[get_product(item_id) for item_id in json.loads(data["contents"])],
+            cost=data["cost"],
+        )
 
 
-def get_cart(username: str) -> list:
+def get_cart(username: str) -> List[Product]:
+    """
+    Fetches the cart contents for a given username.
+
+    Args:
+        username (str): The username of the user.
+
+    Returns:
+        List[Product]: A list of Product objects in the user's cart.
+    """
     cart_details = dao.get_cart(username)
-    if cart_details is None:
+    if not cart_details:
         return []
-    
-    items = []
+
+    products_in_cart = []
     for cart_detail in cart_details:
-        contents = cart_detail['contents']
-        evaluated_contents = eval(contents)  
-        for content in evaluated_contents:
-            items.append(content)
-    
-    i2 = []
-    for i in items:
-        temp_product = products.get_product(i)
-        i2.append(temp_product)
-    return i2
+        contents = json.loads(cart_detail["contents"])
+        products_in_cart.extend(get_product(item_id) for item_id in contents)
 
-    
+    return products_in_cart
 
 
-def add_to_cart(username: str, product_id: int):
+def add_to_cart(username: str, product_id: int) -> None:
+    """
+    Adds a product to the user's cart.
+
+    Args:
+        username (str): The username of the user.
+        product_id (int): The ID of the product to add.
+    """
     dao.add_to_cart(username, product_id)
 
 
-def remove_from_cart(username: str, product_id: int):
+def remove_from_cart(username: str, product_id: int) -> None:
+    """
+    Removes a product from the user's cart.
+
+    Args:
+        username (str): The username of the user.
+        product_id (int): The ID of the product to remove.
+    """
     dao.remove_from_cart(username, product_id)
 
-def delete_cart(username: str):
+
+def delete_cart(username: str) -> None:
+    """
+    Deletes the user's cart.
+
+    Args:
+        username (str): The username of the user.
+    """
     dao.delete_cart(username)
-
-
